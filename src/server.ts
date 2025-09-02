90e9c9d1858725ea8722c7a605ddc9de775c6f82
mdkamil-IT <md.kamil@miritech.com>
2025-09-02 17:21:50 +0530

    add looger winston

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import app from './app.ts';
 import { Config } from './config/index.ts';
+import logger from './config/logger.ts';
 
 const startServer = () => {
    const PORT = Config.PORT || 3000;
    try {
       app.listen(PORT, () => {
-         console.log(` Server is running on port ${PORT}`);
+         //  console.log(` Server is running on port ${PORT}`);
+         logger.info(`Server is running on port ${PORT}`);
       });
    } catch (error) {
-      console.error('Failed to start server:', error);
+      // console.error('Failed to start server:', error);
+      logger.error('Failed to start server:', error);
       process.exit(1);
    }
 };
