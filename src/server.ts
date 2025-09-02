cc3a25b50e2702a896064c61feb3666cdf83d82c
mdkamil-IT <md.kamil@miritech.com>
2025-09-02 16:09:12 +0530

    add  express server with nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-import { Config } from './config/index';
+import app from './app.ts';
+import { Config } from './config/index.ts';
 
-console.log(Config.PORT);
+const startServer = () => {
+   const PORT = Config.PORT || 3000;
+   try {
+      app.listen(PORT, () => {
+         console.log(` Server is running on port ${PORT}`);
+      });
+   } catch (error) {
+      console.error('Failed to start server:', error);
+      process.exit(1);
+   }
+};
+
+startServer();
