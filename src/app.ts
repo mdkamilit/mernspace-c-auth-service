1f3dc141dfb09a525702b120adc96fd8da4e3f5d
mdkamil-IT <md.kamil@miritech.com>
2025-09-04 22:04:24 +0530

    add test cases for user

--- src/app.ts
+++ src/app.ts
@@ -17,13 +17,11 @@ app.get('/', (req, res) => {
    res.status(200).send('Welcome to our service');
 });
 
- 
 app.use('/auth', authRouter);
 
 //Global middleware error handler
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-    
    logger.error('Global error handler:', err.message);
    const statusCode = err.statusCode || 500;
    res.status(statusCode).json({
